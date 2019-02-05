d5ccab7c1243c2dbfd29e18716ec738e60f8c6f8
Waldemar Mozola <waldemar.mozola@onet.pl>
2019-02-05 22:39:16 +0100

    BubblesortMethode

---
+++ src/main/java/company/com/BubbleSort.java
@@ -0,0 +1,27 @@
+package company.com;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static int[] bubblesortMethode(int sortMe[]) {
+        int nrOfSwaps = 0;
+
+        for (int i = 0; i < sortMe.length - 1; i++) {
+            for (int j = i + 1; j < sortMe.length; j++) {
+                if (sortMe[i] > sortMe[j]) {
+                    int temp = sortMe[j];
+                    sortMe[j] = sortMe[i];
+                    sortMe[i] = temp;
+                }
+            }
+            nrOfSwaps++;
+        }
+        System.out.println("Number of swaps" + " " + nrOfSwaps);
+        return sortMe;
+    }
+
+    public static void main(String[] args) {
+        int sortMe[] = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(bubblesortMethode(sortMe)));
+    }
+}
